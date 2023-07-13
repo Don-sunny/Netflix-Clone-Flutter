07290c7e1109e39650927493a265b71ffe12e48f
Don-sunny <donsunny544@gmail.com>
2023-07-13 13:18:05 +0530

    Bottom Navigation Bar Completed

---
+++ lib/presentation/main_page/widgets/bottom_nav.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+ValueNotifier<int> indexChangeNotifier = ValueNotifier(0);
+
+class BottomNavigationWidget extends StatelessWidget {
+  const BottomNavigationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: indexChangeNotifier,
+      builder: (BuildContext context, int newIndex, _) {
+        return BottomNavigationBar(
+          onTap: (index) {
+            indexChangeNotifier.value = index;
+          },
+          currentIndex: newIndex,
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+          selectedIconTheme: const IconThemeData(
+            color: Colors.white,
+          ),
+          unselectedIconTheme: const IconThemeData(
+            color: Colors.grey,
+          ),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.collections),
+              label: 'New & Hot',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.emoji_emotions),
+              label: 'Fast laughs',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.download),
+              label: 'Downloads',
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ lib/presentation/main_page/widgets/screen_main_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_project/presentation/downloads/widgets/scareen_downloads.dart';
+import 'package:netflix_project/presentation/fast_laugh/screen_fast_laugh.dart';
+import 'package:netflix_project/presentation/home/screen_home.dart';
+import 'package:netflix_project/presentation/main_page/widgets/bottom_nav.dart';
+import 'package:netflix_project/presentation/new_and_hot/screen_new_and_%20hot.dart';
+import 'package:netflix_project/presentation/search/screen_search.dart';
+
+class ScreenMainPage extends StatelessWidget {
+  ScreenMainPage({super.key});
+
+  final _pages = [
+    const ScreenHome(),
+    const ScreenNewAndHot(),
+    const ScreenFastLaugh(),
+    const ScreenSearch(),
+    const ScreenDownloads(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ValueListenableBuilder(
+        valueListenable: indexChangeNotifier,
+        builder: (BuildContext context, int index, _) {
+          return _pages[index];
+        },
+      ),
+      bottomNavigationBar: const BottomNavigationWidget(),
+    );
+  }
+}
